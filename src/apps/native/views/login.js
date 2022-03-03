c41bc40f1d1792e9c5be20d94348e420414ca04e
Sebastian James <sebaxj@stanford.edu>
2022-03-03 00:31:54 -0800

    sebs changes to login screen

--- src/apps/native/views/login.js
+++ src/apps/native/views/login.js
@@ -1,21 +1,137 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import {
+	useFonts,
+	Montserrat_100Thin,
+	Montserrat_200ExtraLight,
+	Montserrat_300Light,
+	Montserrat_400Regular,
+	Montserrat_500Medium,
+	Montserrat_600SemiBold,
+	Montserrat_700Bold,
+	Montserrat_800ExtraBold,
+	Montserrat_900Black,
+	Montserrat_100Thin_Italic,
+	Montserrat_200ExtraLight_Italic,
+	Montserrat_300Light_Italic,
+	Montserrat_400Regular_Italic,
+	Montserrat_500Medium_Italic,
+	Montserrat_600SemiBold_Italic,
+	Montserrat_700Bold_Italic,
+	Montserrat_800ExtraBold_Italic,
+	Montserrat_900Black_Italic,
+} from '@expo-google-fonts/montserrat';
 
 const LoginScreen = () => {
-	return (
-		<View style={styles.container}>
-			<Text>Login Screen</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+	let [fontsLoaded] = useFonts({
+		Montserrat_100Thin,
+		Montserrat_200ExtraLight,
+		Montserrat_300Light,
+		Montserrat_400Regular,
+		Montserrat_500Medium,
+		Montserrat_600SemiBold,
+		Montserrat_700Bold,
+		Montserrat_800ExtraBold,
+		Montserrat_900Black,
+		Montserrat_100Thin_Italic,
+		Montserrat_200ExtraLight_Italic,
+		Montserrat_300Light_Italic,
+		Montserrat_400Regular_Italic,
+		Montserrat_500Medium_Italic,
+		Montserrat_600SemiBold_Italic,
+		Montserrat_700Bold_Italic,
+		Montserrat_800ExtraBold_Italic,
+		Montserrat_900Black_Italic,
+	});
+
+	// state to keep track of email and password fields
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	} else {
+		return (
+			<View style={[styles.container, { flexDirection: 'column' }]}>
+				<View style={{ marginTop: 100, marginBottom: 30 }}>
+					<Image style={styles.picture} source={require('../assets/soundwave.png')} />
+				</View>
+				<View style={styles.inputView}>
+					<TextInput
+						style={styles.textInput}
+						placeholder="Email or Username"
+						onChangeText={(email) => setEmail(email)}
+					/>
+				</View>
+				<View style={styles.inputView}>
+					<TextInput
+						style={styles.textInput}
+						placeholder="Password"
+						secureTextEntry={true}
+						onChangeText={(password) => setPassword(password)}
+					/>
+				</View>
+				<TouchableOpacity>
+					<Text style={styles.forgotButton}>Forgot Password?</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.loginButton}>
+					<Text style={styles.loginText}>Login</Text>
+				</TouchableOpacity>
+				<StatusBar style="auto" />
+			</View>
+		);
+	}
 };
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
+		alignItems: 'center', // aligns items in the x direction
+		justifyContent: 'flex-start', // aligns items in the y direction
+	},
+	name: {
+		fontFamily: 'Montserrat_700Bold',
+		fontSize: 28,
+		fontWeight: 'bold',
+	},
+	picture: {
+		resizeMode: 'contain',
+		height: 100,
+	},
+	inputView: {
+		backgroundColor: '#FF84849c',
+		borderRadius: 30,
+		width: '70%',
+		height: 45,
+		marginBottom: 20,
+	},
+	textInput: {
+		height: 50,
+		flex: 1,
+		padding: 10,
+		marginLeft: 20,
+		fontFamily: 'Montserrat_600SemiBold',
+	},
+	forgotButton: {
+		height: 30,
+		marginBottom: 30,
+		fontFamily: 'Montserrat_800ExtraBold',
+	},
+	loginButton: {
+		borderRadius: 30,
+		width: '70%',
+		height: 45,
+		marginBottom: 20,
 		alignItems: 'center',
 		justifyContent: 'center',
+		backgroundColor: '#a1cddbff',
+	},
+	loginText: {
+		fontFamily: 'Montserrat_800ExtraBold',
+		fontSize: 20,
 	},
 });
 
