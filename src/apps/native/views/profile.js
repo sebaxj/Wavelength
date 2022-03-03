1b814d3ed125bf2441f877a4131ff4ee506978c6
Connor <cohogan@stanford.edu>
2022-03-03 13:12:34 -0800

    song formatting on login page

--- src/apps/native/views/profile.js
+++ src/apps/native/views/profile.js
@@ -29,14 +29,22 @@ import {
 } from '@expo-google-fonts/montserrat';
 
 const formatPlaylist = () => {
-    // let output = connorData.items
-    let output = connorData.items.map((track) => {
-        return <Image style={styles.picture} source={track.track.album.images[0].url} key={track.track.album.id} />;
-        // console.log(track.track.album.images[0].url)
-    });
-    // console.log(output)
-    return output;
-};
+	// let output = connorData.items
+	let output = connorData.items.map( track => {
+		return (
+			<View style={{ marginTop: 5, marginBottom: 5, flexDirection: 'row', width: '100%' }}>
+				<Image style={styles.album_picture} source={track.track.album.images[0].url} key={track.track.album.id} />
+				<View style={{alignItems: 'flex-start', width: '80%'}}>
+					<Text numberOfLines={1} style={{fontSize: 16}}>{track.track.name}</Text>
+					<Text numberOfLines={1}>{track.track.artists[0].name}</Text>
+				</View>
+			</View>
+		)
+		// console.log(track.track.album.images[0].url)
+	})
+	// console.log(output)
+	return output
+}
 
 const ProfileScreen = () => {
     let [fontsLoaded] = useFonts({
@@ -73,29 +81,32 @@ const ProfileScreen = () => {
         return () => (mounted = false);
     }, []);
 
-    // formatPlaylist()
 
     // api.getUserById('621e9e6c9429c44dc023cfae').then( output => {
     // 	const loadedUser = output.data.data
     // 	setUser(loadedUser)
     // }).catch (err => console.log(err))
 
-    if (!fontsLoaded) {
-        return <AppLoading />;
-    } else {
-        return (
-            <View style={[styles.container, { flexDirection: 'column', flex: 1 }]}>
-                <View style={{ marginTop: 30, marginBottom: 30 }}>
-                    <Image style={styles.picture} source={user.avatar} />
-                </View>
-                <View style={{ marginTop: 20, marginBottom: 20 }}>
-                    <Text style={styles.name}>{user.user_name}</Text>
-                </View>
-                <View style={{ marginTop: 20, marginBottom: 80 }}>
-                    <Text>{user.location}</Text>
-                </View>
-                <View style={{ marginTop: 20, flex: 1 }}>
-                    {/* <FlatList
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	} else {
+		return (
+			// <View style={[styles.container, { flexDirection: 'column', flex: 1 }]}>
+				<ScrollView contentContainerStyle={styles.container}>
+				<View style={{ marginTop: 30, marginBottom: 30 }}>
+					<Image style={styles.picture} source={user.avatar} />
+				</View>
+				<View style={{ marginTop: 20, marginBottom: 20 }}>
+					<Text style={styles.name}>{user.user_name}</Text>
+				</View>
+				<View style={{ marginTop: 20, marginBottom: 30 }}>
+					<Text>{user.location}</Text>
+				</View>
+				<View style={{ marginTop: 20, marginBottom: 30 }}>
+					<Text>Liked Songs</Text>
+				</View>
+				{/* <View style={{ marginTop: 20, flex: 1 }}> */}
+					{/* <FlatList
 						data = {connorData.items}
 						renderItem = {(track) => {
 							track = track.item
@@ -104,32 +115,42 @@ const ProfileScreen = () => {
 						}
 					}
 					/> */}
-                    <ScrollView>{formatPlaylist()}</ScrollView>
-                </View>
-                <StatusBar style="auto" />
-            </View>
-        );
-    }
+					{/* <ScrollView> */}
+						{formatPlaylist()}
+					{/* </ScrollView> */}
+				{/* </View> */}
+				<StatusBar style="auto" />
+				</ScrollView>
+			// </View>
+		);
+	}
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center', // aligns items in the x direction
-        justifyContent: 'flex-start', // aligns items in the y direction
-    },
-    name: {
-        fontFamily: 'Montserrat_700Bold',
-        fontSize: 28,
-        fontWeight: 'bold',
-    },
-    picture: {
-        resizeMode: 'contain',
-        height: 250,
-        width: 250,
-        borderRadius: 250 / 2,
-    },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center', // aligns items in the x direction
+		justifyContent: 'flex-start', // aligns items in the y direction
+	},
+	name: {
+		fontFamily: 'Montserrat_700Bold',
+		fontSize: 28,
+		fontWeight: 'bold',
+	},
+	picture: {
+		resizeMode: 'contain',
+		height: 250,
+		width: 250,
+		borderRadius: 250 / 2,
+	},
+	album_picture: {
+		resizeMode: 'contain',
+		height: 50,
+		width: 50,
+		marginRight: 10,
+		marginLeft: 10,
+	},
 });
 
 export default ProfileScreen;
